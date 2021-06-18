50af8d792085b1a950b107250d06b14be943168c
Chris McIntosh <chocrates@github.com>
2021-06-18 15:27:18 -0500

    moving to env vars cuz this didn't work

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,11 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" { }
-variable "tenant_id" {}
+variable "resource_group_name" {}
 
 # Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.26"
+      version = ">= 2.46.0"
     }
   }
 
@@ -24,18 +21,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = "westus2"
   tags = {
-    Environment = "prod"
+    Environment = "dev"
     Team        = "DevOps"
   }
 }
