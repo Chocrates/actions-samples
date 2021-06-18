b0bc853dd2fd7f65e27661de591cb7fd96f1351a
Chris McIntosh <chocrates@github.com>
2021-06-18 14:49:08 -0500

    Using SP creds

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {
+}
+variable "tenant_id" {}
+variable "resource_group_name" {}
+
 # Configure the Azure provider
 terraform {
   required_providers {
@@ -8,7 +15,7 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "chocrates-rg"
+    resource_group_name  = var.resource_group_name
     storage_account_name = "tfstatechocrates"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
@@ -19,6 +26,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,5 @@
 variable "resource_group_name" {}
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
